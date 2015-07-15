28c84d47ee7950e628fb78185d481d2fd1009d42
xcjs <zlowery@xcjs.com>
2015-07-15 18:47:29 -0400

    Added solution for challenge #4. Things are still pretty simple at this point, and I rely on random values.

--- elevator.js
+++ elevator.js
@@ -1,26 +1,30 @@
 {
     init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
+        var getRandomIntInclusive = function(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+        
+        elevators.forEach(function(elevator) {
+            // Whenever the elevator is idle (has no more queued destinations) ...
+            elevator.on("idle", function() {
+
+            });
+
+            elevator.on("floor_button_pressed", function(floorNum) {            
+                elevator.goToFloor(floorNum, true);
+            });
+        });
 
         floors.forEach(function(floor) {
             floor.on("up_button_pressed", function() {
                 // Maybe tell an elevator to go to this floor?
-                elevator.goToFloor(floor.floorNum());
+                elevators[getRandomIntInclusive(0, elevators.length - 1)].goToFloor(floor.floorNum());
             });
 
             floor.on("down_button_pressed", function() {
                 // Maybe tell an elevator to go to this floor?
-                elevator.goToFloor(floor.floorNum());
+                elevators[getRandomIntInclusive(0, elevators.length - 1)].goToFloor(floor.floorNum());
             });
-        });
-        
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            
-        });
-        
-        elevator.on("floor_button_pressed", function(floorNum) {            
-            elevator.goToFloor(floorNum, true);
         });
     },
     update: function(dt, elevators, floors) {
