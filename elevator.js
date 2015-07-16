363520890c04e087d0c9f069defeb68ae6b3f759
xcjs <zlowery@xcjs.com>
2015-07-15 20:25:25 -0400

    Implemented crude queue rearrangement.

--- elevator.js
+++ elevator.js
@@ -2,24 +2,92 @@
     init: function(elevators, floors) {
         var lastSelectedElevator = -1;
         
+        var directions = {
+            down: 'down',
+            up: 'up'
+        };
+        
+        var lastElevatorDirection = directions.down;
+
         var getElevator = function() {
             if(lastSelectedElevator >= elevators.length - 1) {
                 lastSelectedElevator = 0;    
             } else {
                 lastSelectedElevator++;
             }
-            
+
             return lastSelectedElevator;
-        };       
+        };
         
+        var getElevatorDirection = function() {
+            lastElevatorDirection;
+            
+            if(lastElevatorDirection === directions.down) {
+                lastElevatorDirection = directions.up;
+            } else {
+                lastElevatorDirection = directions.down;
+            }
+            
+            return lastElevatorDirection;
+        };
+        
+        var rearrangeQueue = function(currentFloor, floorQueue, dir) {
+            if(floorQueue.length === 0) return [];
+
+            var splitIndex = -1;
+
+            var aboveFloors = [];
+            var belowFloors = [];
+
+            var newQueue = null
+
+            floorQueue.foreach(function(floor) {
+                if(floor <= currentFloor) {
+                    belowFloors.push(floor);
+                } else {
+                    aboveFloors.push(floor);
+                }
+            });            
+
+            if(dir === directions.up) {
+                newQueue = aboveFloors.concat(belowFloors);
+            } else {
+                newQueue = belowFloors.concat(aboveFloors);
+            }
+
+            return newQueue;
+        };
+        
+        var setElevatorQueue = function(elevator) {
+            var pressedFloors = elevator.getPressedFloors();
+            var pos = elevator.currentFloor();
+            
+            if(pressedFloors.length === 0) return;
+            
+            pressedFloors.sort();
+
+            var dir = getElevatorDirection();
+
+            if(dir === directions.down) {
+                pressedFloors.reverse();
+            } 
+            
+            var newQueue = rearrangeQueue(pos, pressedFloors, dir);
+            
+            elevator.destinationQueue = newQueue;
+            elevator.checkDestinationQueue();
+        };
+
         elevators.forEach(function(elevator) {
-            // Whenever the elevator is idle (has no more queued destinations) ...
+            // Whenever the elevator is idle (has no more queued destinations) ...          
+            
             elevator.on("idle", function() {
 
             });
 
-            elevator.on("floor_button_pressed", function(floorNum) {            
-                elevator.goToFloor(floorNum, true);
+            elevator.on("floor_button_pressed", function(floorNum) {               
+                elevator.goToFloor(floorNum);
+                rearrangeQueue(elevator);
             });
         });
 
@@ -33,7 +101,8 @@
             });
         });
     },
+    
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
-    }
+    }   
 }
